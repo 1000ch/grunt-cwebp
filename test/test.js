5e03abc3e1b7b3669aa9a96938e9f77a2f240367
1000ch <shogo.sensui@gmail.com>
2014-06-09 10:14:41 +0900

    existsSync

--- test/test.js
+++ test/test.js
@@ -5,12 +5,12 @@ var fs = require('fs');
 exports.cwebp = {
   convertPNG: function (test) {
     test.expect(1);
-    test.ok(fs.statSync('tmp/test-png.webp').isFile(), 'should convert PNG images');
+    test.ok(fs.existsSync('tmp/test-png.webp'), 'should convert PNG images');
     test.done();
   },
   convertJPG: function (test) {
     test.expect(1);
-    test.ok(fs.statSync('tmp/test-jpg.webp').isFile(), 'should convert JPG images');
+    test.ok(fs.existsSync('tmp/test-jpg.webp'), 'should convert JPG images');
     test.done();
   }
 };
